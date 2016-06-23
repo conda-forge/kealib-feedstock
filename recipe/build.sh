3dd84eac0f5840ebe23a7914006bc1a7949f9633
Sam Gillingham <gillingham.sam@gmail.com>
2016-06-23 17:20:51 +1000

    Use toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
 cd trunk
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
 -D CMAKE_SKIP_RPATH=ON \
