e4c99727770f2a72feaf119532b5c50901b34936
Sam Gillingham <gillingham.sam@gmail.com>
2018-05-25 13:26:58 +1000

    added missing changed files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,12 @@
 cd trunk
+
+mkdir build
+cd build
+
 cmake -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
--D HDF5_INCLUDE_DIR=%LIBRARY_PREFIX%\include ^
--D HDF5_LIB_PATH=%LIBRARY_PREFIX%\lib ^
+-D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
 -D CMAKE_BUILD_TYPE=Release ^
--G "NMake Makefiles" .
+-G "NMake Makefiles" ..
 if errorlevel 1 exit 1
 
 nmake
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-if [ `uname` == Darwin ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-fi
-
 cd trunk
+
+mkdir build
+cd build
+
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
--D CMAKE_SKIP_RPATH=ON \
--D HDF5_INCLUDE_DIR=$PREFIX/include \
--D HDF5_LIB_PATH=$PREFIX/lib \
-.
+-D CMAKE_PREFIX_PATH=$PREFIX \
+..
 
 make
 make install
 make test
 
-# hack so the old 1.4.6 .so still exists for old builds. Remove in next version...
-if [ `uname` == Darwin ]; then
-    ln -s $PREFIX/lib/libkea.1.4.7.dylib $PREFIX/lib/libkea.1.4.6.dylib
-else
-    ln -s $PREFIX/lib/libkea.so.1.4.7 $PREFIX/lib/libkea.so.1.4.6
-fi
