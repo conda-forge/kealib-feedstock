e040083b69888bad7da9266e3d028cbaefc4b2dc
Sam Gillingham <gillingham.sam@gmail.com>
2023-05-16 14:06:40 +1000

    attempt to also build Python bindings

---
+++ recipe/bld_python.bat
@@ -0,0 +1,15 @@
+cd python
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% \
+    -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ..
+if errorlevel 1 exit 1
+    
+nmake
+if errorlevel 1 exit 1
+nmake install
+if errorlevel 1 exit 1
+ctest --output-on-failure
+if errorlevel 1 exit 1
---
+++ recipe/build_python.sh
@@ -0,0 +1,14 @@
+
+cd python
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
+    -D CMAKE_PREFIX_PATH=$CONDA_PREFIX ..
+    
+make
+make install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+ctest --output-on-failure
+fi
