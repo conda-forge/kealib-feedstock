eec3fe88962627d08f7f1e71ca2cf58ca0a67836
Sam Gillingham <gillingham.sam@gmail.com>
2023-05-17 16:49:51 +1000

    try version hack

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -1,14 +1,22 @@
 
 set -xe
-env
+
 cd python
 rm -rf build
 mkdir build
 cd build
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    # determine awkward version using some magic rather than importing (which fails due to binary modules)
+    VER=`grep __version__ ${STDLIB_DIR}/site-packages/awkward/_version.py | awk '{print $NF}' | tr -d "'"`
+    PLATFORM_OPTS="-D AWKWARD_VERSION=$VER"
+else
+    PLATFORM_OPTS=""
+fi
+
 cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
-    -D CMAKE_PREFIX_PATH=$CONDA_PREFIX ..
+    -D CMAKE_PREFIX_PATH=$CONDA_PREFIX $PLATFORM_OPTS ..
     
 make
 make install
