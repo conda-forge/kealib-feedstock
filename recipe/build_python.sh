7e33e98600314faf83ebd8e6f8727cbee27b1e2e
Sam Gillingham <gillingham.sam@gmail.com>
2023-05-18 08:31:33 +1000

    don't run test on emulation

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -8,6 +8,7 @@ cd build
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     # determine awkward version using some magic rather than importing (which fails due to binary modules)
+    # Probably if I was better at working out the emulation stuff this wouldn't be needed...
     VER=`grep __version__ ${STDLIB_DIR}/site-packages/awkward/_version.py | awk '{print $NF}' | tr -d "'"`
     PLATFORM_OPTS="-D AWKWARD_VERSION=$VER"
 else
@@ -20,6 +21,6 @@ cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release \
     
 make
 make install
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
     ctest --output-on-failure
 fi
