982605880926175971b1a37c4b4835693a553ae7
akrherz <akrherz@iastate.edu>
2024-10-25 09:40:35 -0500

    try older compiler on osx, revert debug

--- recipe/install_lib.sh
+++ recipe/install_lib.sh
@@ -2,5 +2,5 @@
 cd build
 make install
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-    make test || ctest --rerun-failed --output-on-failure
+    make test
 fi
