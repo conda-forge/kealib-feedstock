d7582a90092267d8e06e99f61cdde857f4fce11d
Isuru Fernando <isuruf@gmail.com>
2024-11-04 07:24:49 -0800

    Update install_lib.sh

--- recipe/install_lib.sh
+++ recipe/install_lib.sh
@@ -3,4 +3,6 @@ cd build
 make install
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
     CTEST_OUTPUT_ON_FAILURE=1 make test
+    $OTOOL -L ./src/test1
+    $OTOOL -l ./src/test1
 fi
