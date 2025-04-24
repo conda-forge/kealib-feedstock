5600dcd7f86f1c9f30db43ef75cfcccbc7c62847
Sam Gillingham <gillingham.sam@gmail.com>
2025-04-24 18:56:41 +1000

    skip test on OSX

--- recipe/install_lib.sh
+++ recipe/install_lib.sh
@@ -1,6 +1,6 @@
 
 cd build
 make install
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") && ($(uname) != "Darwin") ]]; then
     make test
 fi
