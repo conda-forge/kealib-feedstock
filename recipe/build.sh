4a1a3e220c09cbbff3830ea3b5f3327e2c57f214
Sam Gillingham <gillingham.sam@gmail.com>
2025-08-20 09:06:04 +1000

    get quotes right

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ cd build
 EXTRA=
 if [[ $(uname) == "Darwin" ]]; then
     SANITIZE="-fsanitize=address,alignment,unreachable,vla-bound,vptr"
-    EXTRA='-DCMAKE_EXE_LINKER_FLAGS="$SANITIZE" -DCMAKE_CXX_FLAGS="$SANITIZE"'
+    EXTRA="-DCMAKE_EXE_LINKER_FLAGS='$SANITIZE' -DCMAKE_CXX_FLAGS='$SANITIZE'"
 fi
 
 cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
