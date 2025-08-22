6440f77e9f93fc726a7676097945de1e0b681479
Sam Gillingham <gillingham.sam@gmail.com>
2025-08-22 14:04:31 +1000

    --output-on-failure

--- recipe/install_lib.sh
+++ recipe/install_lib.sh
@@ -3,5 +3,5 @@ cd build
 make install
 if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") ]]; then
     export ASAN_OPTIONS="verbosity=1:malloc_context_size=20"
-    make test
+    ctest --output-on-failure
 fi
